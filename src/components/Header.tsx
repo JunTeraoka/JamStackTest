425129f1fe086ae7543d259eca5c22dc9704c3b5
Jun-T-git <jun0303t@icloud.com>
2022-02-04 17:53:18 +0900

    カテゴリ一覧の表示

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from "scss/components/Header.module.scss";
 import Link from "next/link";
 import { client, MenuLocationEnum } from "client";
@@ -12,10 +12,7 @@ function Header({
   title = "Headless by WP Engine",
   description,
 }: Props): JSX.Element {
-  const { menuItems } = client.useQuery();
-  const links = menuItems({
-    where: { location: MenuLocationEnum.PRIMARY },
-  }).nodes;
+  const categories = client.useQuery().categories();
 
   return (
     <header>
@@ -30,18 +27,18 @@ function Header({
         </div>
         <div className={styles.menu}>
           <ul>
-            {links?.map((link) => (
-              <li key={`${link.label}$-menu`}>
-                <Link href={link.url ?? ""}>
-                  <a href={link.url}>{link.label}</a>
+            {categories.nodes.map((category) => (
+              <li key={`${category.id}$-menu`}>
+                <Link href={`/category/${category.slug}`}>
+                  <a href={`/category/${category.slug}`}>{category.name}</a>
                 </Link>
               </li>
             ))}
             <li>
-              <Link href="https://github.com/YukiOkamoto0206/JamStackTest">
+              <Link href="https://github.com/JunTeraoka/JamStackTest">
                 <a
                   className="button"
-                  href="https://github.com/YukiOkamoto0206/JamStackTest"
+                  href="https://github.com/JunTeraoka/JamStackTest"
                 >
                   GitHub
                 </a>
